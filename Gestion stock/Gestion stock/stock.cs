b7cd3924fe21eec5e6509fa6ffc137b5d33b74b3
SaphiraR <saphirarozas@gmail.com>
2016-04-27 15:48:01 +0100

    valid

--- Gestion stock/Gestion stock/stock.cs
+++ Gestion stock/Gestion stock/stock.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Gestion_stock
@@ -15,27 +16,94 @@ namespace Gestion_stock
         public stock()
         {
             InitializeComponent();
-            gestionstkDataContext bdcon = new gestionstkDataContext();
-            
-
+        
         }
 
+
+        private GESTIONLINQDataContext dc = new GESTIONLINQDataContext();
         private void stock_Load(object sender, EventArgs e)
         {
-            // TODO: cette ligne de code charge les données dans la table 'gestionStockDataSet.Produits'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
-            
+            var lesProduits = (from prod in dc.Produits select prod).ToList();
+            foreach (var prod in lesProduits)
+                listeproduitbox.Items.Add(prod);
 
         }
 
         private void Addproduct_Click(object sender, EventArgs e)
         {
-            newproduct addproduct = new newproduct();
-            addproduct.Show();
+            int anInteger;
+            anInteger = Convert.ToInt32(textBox2.Text);
+            anInteger = int.Parse(textBox2.Text);
+            double value = double.Parse(textBox3.Text);
+            Produits addprod = new Produits
+            {
+
+                labelle_produits = textBox1.Text,
+                qtestock_produits = anInteger,
+                prixvente_produits = value,
+                description_produits = textBox4.Text,
+                famille_produits = textBox5.Text,
+            };
+            dc.Produits.InsertOnSubmit(addprod);
+            try
+            {
+                dc.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void buttonretoure_Click(object sender, EventArgs e)
         {
+            this.Hide();
+            Acceuil retourAcc = new Acceuil();
+            retourAcc.Show();
+        }
+
+        private void delt_Click(object sender, EventArgs e)
+        {
+            Produits leProduit = listeproduitbox.SelectedItem as Produits;
+            var aSup = (from prod in dc.Produits
+                        where prod.id_produits == leProduit.id_produits
+                        select prod);
+            foreach (var prod in aSup)
+            {
+                dc.Produits.DeleteOnSubmit(prod);
+            }
+
+            try
+            {
+                dc.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void modifier_Click(object sender, EventArgs e)
+        {
+            Produits leProduitSel = listeproduitbox.SelectedItem as Produits;
+            var aMod = (from prod in dc.Produits
+                        where prod.id_produits == leProduitSel.id_produits
+                        select prod);
+            foreach (var prod in aMod)
+            {
+                textBox1.Text = prod.labelle_produits;
+                textBox2.Text = prod.qtestock_produits.ToString();
+                textBox3.Text = prod.prixvente_produits.ToString();
+                textBox4.Text = prod.description_produits;
+                textBox5.Text = prod.famille_produits;
+            }
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
             
+
         }
     }
 }
